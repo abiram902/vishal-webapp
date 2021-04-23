bc4975a662ccb5a96a8188c9a7e6b2b39c5a748a
abiram902 <abiram902@gmail.com>
2021-04-23 18:43:40 +0530

    removed context

--- src/index.js
+++ src/index.js
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import StateProvider from "./StateProvider";
-import Reducer, { initialState } from "./Reducer";
 
 ReactDOM.render(
   <React.StrictMode>
-    <StateProvider reducer={Reducer} initialState={initialState}>
-      <App />
-    </StateProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
